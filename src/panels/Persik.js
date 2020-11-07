ddd71ac39d12cc240752411326caa62d6b8707af
Viacheslav <nishiy@pop-os.localdomain>
2020-11-07 21:32:33 +0300

    naigralsya for today

--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import Async from 'react-async';
 import PropTypes from 'prop-types';
 import { platform, IOS, Text} from '@vkontakte/vkui';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import Card from '@vkontakte/vkui/dist/components/Card/Card';
 import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
+import { properties } from '../properties.js';
 
 import persik from '../img/persik.png';
 import './Persik.css';
 
 const osName = platform();
 
+const loadCards = () =>
+  fetch(properties.host + "/api/cards")
+    .then(response => (response.ok ? response : Promise.reject(response)))
+	.then(response => response.json())
+	
 const Persik = props => (
 	<Panel id={props.id}>
 		<PanelHeader
@@ -22,7 +30,27 @@ const Persik = props => (
 			Начало игры
 		</PanelHeader>
 		<img className="Persik" src={persik} alt="Persik The Cat"/>
-		<Text className={'cringe'}>Кринджилл...</Text>
+		<Card size="l" mode="outline">
+			<div style={{ height: 96 }}>
+				<Async promiseFn={loadCards}>
+					{({ data, err, isLoading }) => {
+					if (isLoading) return "Loading..."
+					if (err) return `Something went wrong: ${err.message}`
+
+					if (data)
+						return (
+						<div>
+							{data.map(card=> (
+							<div>
+								<p>{card}</p>
+							</div>
+							))}
+						</div>
+						)
+					}}
+				</Async>
+			</div>
+        </Card>
 	</Panel>
 );
 
