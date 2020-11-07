590cdccd830cfb305bfe262c6e8587825ed77bda
Andrey Klyuev <Klyuev_Andrey@mail.ru>
2020-11-07 23:42:29 +0300

    fixed sign

--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Async from 'react-async';
 import PropTypes from 'prop-types';
-import { platform, IOS, Text} from '@vkontakte/vkui';
+import { platform, IOS, Text, Card, Spinner, Div } from '@vkontakte/vkui';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
-import Card from '@vkontakte/vkui/dist/components/Card/Card';
 import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
@@ -20,39 +19,44 @@ const loadCards = () =>
     .then(response => (response.ok ? response : Promise.reject(response)))
 	.then(response => response.json())
 	
-const Persik = props => (
-	<Panel id={props.id}>
-		<PanelHeader
-			left={<PanelHeaderButton onClick={props.go} data-to="home">
-				{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
-			</PanelHeaderButton>}
-		>
-			Начало игры
-		</PanelHeader>
-		<img className="Persik" src={persik} alt="Persik The Cat"/>
-		<Card size="l" mode="outline">
-			<div style={{ height: 96 }}>
-				<Async promiseFn={loadCards}>
-					{({ data, err, isLoading }) => {
-					if (isLoading) return "Loading..."
-					if (err) return `Something went wrong: ${err.message}`
+const Persik = props => {
+	//const [activePanel, setActivePanel] = useState('home');
 
-					if (data)
-						return (
-						<div>
-							{data.map(card=> (
-							<div>
-								<p>{card}</p>
-							</div>
-							))}
-						</div>
-						)
-					}}
-				</Async>
-			</div>
-        </Card>
-	</Panel>
-);
+	return (
+		<Panel id={props.id}>
+			<PanelHeader
+				left={<PanelHeaderButton onClick={props.go} data-to="home">
+					{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
+				</PanelHeaderButton>}
+			>
+				Начало игры
+			</PanelHeader>
+				<Div className={'cardDiv'}>
+					<Card className={'card'} size="m" mode="outline">
+						<Div style={{height: 96}}>
+							<Async promiseFn={loadCards}>
+								{({data, err, isLoading}) => {
+									if (isLoading) return <Spinner/>
+									if (err) return `Something went wrong: ${err.message}`
+
+									if (data)
+										return (
+											<div>
+												{data.map(card => (
+													<div>
+														<p>{card}</p>
+													</div>
+												))}
+											</div>
+										)
+								}}
+							</Async>
+						</Div>
+					</Card>
+				</Div>
+		</Panel>
+	);
+}
 
 Persik.propTypes = {
 	id: PropTypes.string.isRequired,
