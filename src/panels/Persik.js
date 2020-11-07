d30e4cebc51a669bc30c3c5fbdf730734d99dccc
Andrey Klyuev <Klyuev_Andrey@mail.ru>
2020-11-07 16:53:20 +0300

    Initial commit

---
+++ src/panels/Persik.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { platform, IOS, Text} from '@vkontakte/vkui';
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton';
+import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
+import Icon24Back from '@vkontakte/icons/dist/24/back';
+
+import persik from '../img/persik.png';
+import './Persik.css';
+
+const osName = platform();
+
+const Persik = props => (
+	<Panel id={props.id}>
+		<PanelHeader
+			left={<PanelHeaderButton onClick={props.go} data-to="home">
+				{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
+			</PanelHeaderButton>}
+		>
+			Начало игры
+		</PanelHeader>
+		<img className="Persik" src={persik} alt="Persik The Cat"/>
+		<Text className={'cringe'}>Кринджил...</Text>
+	</Panel>
+);
+
+Persik.propTypes = {
+	id: PropTypes.string.isRequired,
+	go: PropTypes.func.isRequired,
+};
+
+export default Persik;
